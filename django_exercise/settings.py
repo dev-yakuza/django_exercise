2d3a1f0ac364c9299384f5b04f57f1be91cd4955
dev-yakuza <dev.yakuza@gmail.com>
2019-06-03 11:26:50 +0900

    fix some bug

--- django_exercise/settings.py
+++ django_exercise/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')kr$tx-88&8c*ma@tjp^z&k-9l5efnf0-m)nqh4(u(0mc9oy@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
